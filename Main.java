6c2ecf7a4c4de41d01bb1898a89647ca30b116ea
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-05-23 12:33:10 -0400

    Added code to make test for zero pass

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main {
         if (length < 0 || width < 0 || height < 0) {
             throw new IllegalArgumentException("Length, width, and height must be positive numbers.");
         }   
+        if (length == 0 || width == 0 || height == 0) {
+            throw new IllegalArgumentException("At least one dimension is zero. Shape is no longer a 3D shape therefore volume is zero.");
+        }
         return length * width * height;
     }   
 }
