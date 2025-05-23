565df16d8454b5152ad93bd9bcd3b14632869ea2
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-05-23 11:46:40 -0400

    Method to make the test pass

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static int calculateVolumeRectangularPrism(int length, int width, int height) {
+        return length * width * height;
+    }   
+}
--- TestMain.java
+++ TestMain.java
@@ -3,8 +3,10 @@ public class TestMain {
         testCalculateVolumeRectangularPrism(2, 3, 4, 24);
     }
 
+    // This is a test method for the calculateVolumeRectangularPrism method
+    // It checks if the method returns the expected volume for given dimensions
     private static void testCalculateVolumeRectangularPrism(int length, int width, int height, int expectedVolume) {
-        int actualVolume = calculateVolumeRectangularPrism(length, width, height);
+        int actualVolume = Main.calculateVolumeRectangularPrism(length, width, height);
         if (actualVolume == expectedVolume) {
             System.out.println("Test passed: " + actualVolume);
         } else {
