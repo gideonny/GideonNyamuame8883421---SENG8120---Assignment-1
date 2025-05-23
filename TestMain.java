9cc9b70d0f440fdb258628680755208be2510e0a
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-05-23 11:24:44 -0400

    Innitial TDD test for calculateVlumeRectangularPrism

--- TestMain.java
+++ TestMain.java
@@ -1,3 +1,14 @@
 public class TestMain {
-    
+    public static void main(String[] args) {
+        testCalculateVolumeRectangularPrism(2, 3, 4, 24);
+    }
+
+    private static void testCalculateVolumeRectangularPrism(int length, int width, int height, int expectedVolume) {
+        int actualVolume = calculateVolumeRectangularPrism(length, width, height);
+        if (actualVolume == expectedVolume) {
+            System.out.println("Test passed: " + actualVolume);
+        } else {
+            System.out.println("Test failed: expected " + expectedVolume + ", but got " + actualVolume);
+        }
+    }
 }
